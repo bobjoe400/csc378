19011bc87abd18fbfc56f2bc66eb874bac8221e8
Cooper Mattern <cnamattern@gmail.com>
2025-06-01 10:50:04 -0700

    Level Loading :)

---
+++ Gecko Jump/Assets/Scripts/NextLevelTrigger.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelTrigger : MonoBehaviour
+{
+    [SerializeField] private string sceneToLoad;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            print("Player has entered the door trigger area.");
+
+            SceneManager.LoadScene(sceneToLoad);
+            
+        }
+        else
+        {
+            print("An object other than the player has entered the door trigger area.");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            print("Player has exited the door trigger area.");
+        }
+        else
+        {
+            print("An object other than the player has exited the door trigger area.");
+        }
+    }
+}
---
+++ Gecko Jump/Assets/Scripts/SpawnGerald.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class SpawnGerald : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public CinemachineCamera cinemachineCamera;
+    public GameObject geraldPrefab; // Assign this in the inspector or load from Resources
+
+    void Start()
+    {
+        CreateGerald();
+    }
+
+    void CreateGerald()
+    {
+        // Find the GameObject with the tag "Gerald"
+        GameObject gerald = GameObject.FindGameObjectWithTag("Player");
+
+        // If not found, instantiate a new one
+        if (gerald == null)
+        {
+            Debug.Log("Gerald not found, spawning a new one.");
+            GameObject newGerald = Instantiate(geraldPrefab, transform.position, Quaternion.identity);
+            newGerald.transform.rotation = Quaternion.Euler(0, 0, -90); // Set rotation if needed
+
+            cinemachineCamera.Target.TrackingTarget = newGerald.transform;
+        }
+        else
+        {
+            Debug.Log("Gerald already exists in the scene.");
+        }
+    }
+}
